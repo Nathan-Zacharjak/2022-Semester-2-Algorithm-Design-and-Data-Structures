dee4753a6c3a017b6f5d069715a0b9b608688f69
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-08-11 14:20:47 +0930

    q3 done

--- Tournament.cpp
+++ Tournament.cpp
@@ -20,7 +20,7 @@ Tournament::Tournament() {
 Player* Tournament::RunRound(Player* ply1, Player* ply2) {
     for (int i = 0; i < 5; i++) {
       gameResult = ref.refGame(ply1, ply2);
-
+    cout << gameResult << endl;
       if (gameResult == 'W') {
         ply1Wins++;
       } else if (gameResult == 'L') {
@@ -39,15 +39,15 @@ Player* Tournament::RunRound(Player* ply1, Player* ply2) {
 
 Player* Tournament::run(array<Player*, 8> competitors) {
     cout << "Round 1 winners:" << endl;
-    for (int i = 0; i < 4; i++){
-        winners[i] = RunRound(competitors[i],competitors[i+1]);
+    for (int i = 0; i < 8; i = i + 2){
+        winners.at(i/2) = RunRound(competitors.at(i),competitors.at(i+1));
     }
 
     cout << "Round 2 winners:" << endl;
-    for (int i = 0; i < 2; i++){
-        winners[i] = RunRound(winners[i],winners[i+1]);
+    for (int i = 0; i < 4; i = i + 2){
+        winners.at(i/2) = RunRound(winners.at(i),winners.at(i+1));
     }
 
     cout << "Final winner:" << endl;
-    return RunRound(winners[0],winners[1]);
+    return RunRound(winners.at(0),winners.at(1));
 }
--- main.cpp
+++ main.cpp
@@ -12,16 +12,22 @@ using namespace std;
 
 int main(int argc, char const *argv[]){
     Avalanche* Avalanche1 = new Avalanche;
+    Avalanche* Avalanche2 = new Avalanche;
+    Avalanche* Avalanche3 = new Avalanche;
+    Avalanche* Avalanche4 = new Avalanche;
+    Avalanche* Avalanche5 = new Avalanche;
+    Avalanche* Avalanche6 = new Avalanche;
+    Avalanche* Avalanche7 = new Avalanche;
     Bureaucrat* Bureaucrat1 = new Bureaucrat;
-    Bureaucrat* Bureaucrat2 = new Bureaucrat;
-    Toolbox* Toolbox1 = new Toolbox;
-    Toolbox* Toolbox2 = new Toolbox;
-    Crescendo* Crescendo1 = new Crescendo;
-    Crescendo* Crescendo2 = new Crescendo;
-    FistfullODollars* FistfullODollars1 = new FistfullODollars;
+    // Bureaucrat* Bureaucrat2 = new Bureaucrat;
+    // Toolbox* Toolbox1 = new Toolbox;
+    // Toolbox* Toolbox2 = new Toolbox;
+    // Crescendo* Crescendo1 = new Crescendo;
+    // Crescendo* Crescendo2 = new Crescendo;
+    // FistfullODollars* FistfullODollars1 = new FistfullODollars;
     Tournament* Tournament1 = new Tournament;
 
-    array<Player*,8> competitors = {Avalanche1,Bureaucrat1,Bureaucrat2,Toolbox1,Toolbox2,Crescendo1,Crescendo2,FistfullODollars1};
+    array<Player*,8> competitors = {Avalanche1,Avalanche2,Avalanche3,Avalanche4,Avalanche5,Bureaucrat1,Avalanche6,Avalanche7};
 
     cout << Tournament1->run(competitors)->getName() << endl;
 
