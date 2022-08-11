0e6085ed58a42a2aff1a2a3f2602ee953f35a23a
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-08-11 14:05:46 +0930

    done q3

--- Tournament.cpp
+++ Tournament.cpp
@@ -28,23 +28,26 @@ Player* Tournament::RunRound(Player* ply1, Player* ply2) {
       }
     }
 
-    if (ply1Wins < ply2Wins) {
+    if (ply2Wins <= ply1Wins) {
       winner = ply1;
     } else {
       winner = ply2;
     }
-
+    cout << winner->getName() << endl;
   return winner;
 }
 
 Player* Tournament::run(array<Player*, 8> competitors) {
+    cout << "Round 1 winners:" << endl;
     for (int i = 0; i < 4; i++){
         winners[i] = RunRound(competitors[i],competitors[i+1]);
     }
 
+    cout << "Round 2 winners:" << endl;
     for (int i = 0; i < 2; i++){
         winners[i] = RunRound(winners[i],winners[i+1]);
     }
 
+    cout << "Final winner:" << endl;
     return RunRound(winners[0],winners[1]);
 }
