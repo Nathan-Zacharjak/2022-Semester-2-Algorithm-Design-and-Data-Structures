443ae385b35f2cfdd40f49b44b2e2f4313ec57e1
Nathan-Zacharjak <nathan.zacharjak@student.adelaide.edu.au>
2022-09-14 15:07:23 +0930

    setup prac exam

--- main.cpp
+++ main.cpp
@@ -2,29 +2,8 @@
 
 using namespace std;
 
-int sumDigits(long n, long acc){
-    int lastDigit = n % 10;
-    int nWithoutLastDigit = n / 10;
-
-    cout << lastDigit << " " << nWithoutLastDigit << endl;
-
-    acc = acc + lastDigit;
-
-    if (nWithoutLastDigit == 0){
-        return acc;
-    }
-
-    return sumDigits(nWithoutLastDigit, acc);
-}
-
-int sumDigits(long n){
-    return sumDigits(n, 0);
-}
-
 int main(void){
-    long input = 0;
-    cin >> input;
-    cout << sumDigits(input) << endl;
+    
 
     return 0;
 }
